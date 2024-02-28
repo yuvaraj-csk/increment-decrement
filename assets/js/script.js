5e92aea972f863311bb97a8efb0ce7e730e1806d
yuvaraj-csk <sandhiyayuvaraj08@gmail.com>
2024-02-28 06:36:08 +0530

    first commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,16 @@
 const incrementFunction = () => {
     console.log(`increment logged!`)
+    const numberElement = document.getElementById("counter-number");
+    const number = parseInt(numberElement.textContent);
+    console.log(number);
+    numberElement.innerHTML = number + 1;
+
 }
 
 const decrementFunction = () => {
     console.log(`decrement logged!`)
+    const numberElement = document.getElementById("counter-number");
+    const number = parseInt(numberElement.textContent);
+    console.log(number);
+    numberElement.innerHTML = number - 1;
 }
